28148b4af64882097e3026da1d1384515c27db69
mrsalvini <dsgmcosta@gmail.com>
2019-03-15 16:08:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,40 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :keyword
+
+    def initialize(keyword)
+        @keyword = keyword
+    end
+
+    def match(words)
+        # given an array of words, return an array of the ones that are anagrams of SELF
+        words.select do |word|
+            hashify_word(word) == hashify_word(@keyword)
+        end
+    end
+
+    def hashify_word(word)
+        hashified = {}
+        word.split(//).each do |char|
+            hashified[char] ? hashified[char] += 1 : hashified[char] = 1
+        end
+        hashified
+    end
+
+end
+
+
+# hash1 = {
+#     "a" => 1,
+#     "b" => 6,
+#     "c" => 1,
+#     "d" => 3
+# }
+
+# hash2 = {
+#     "b" => 6,
+#     "c" => 1,
+#     "d" => 3,
+#     "a" => 1
+# }
