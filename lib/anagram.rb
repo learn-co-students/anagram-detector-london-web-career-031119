b91c7f8104f3ff1d80d2229cfdb0f920315af0d1
keoki33 <gwong323@yahoo.com>
2019-03-16 23:32:37 +0000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word
+attr_writer
+attr_reader
+@@all = []
+
+def self.all
+@@all
+end
+
+def initialize(word)
+@word = word
+@@all << self
+end
+
+def match(array)
+new = array.select do |words|
+  word.split("").sort == words.split("").sort
+end
+new
+end
+
+end
+
+# array = %w(gallery ballerina regally clergy largely leading)
+# new =  array.select do |word|
+#   "allergy".split("").sort == word.split("").sort
+# end
+# p new
+# p "allergy".split("").sort
