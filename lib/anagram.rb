b6d280c21d78be47e5d62cc2a39fd74502d24673
peter-keller <keller.f.peter@gmail.com>
2019-03-16 16:53:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+require "pry"
+
+class Anagram
+
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        @matches = @word.split("").sort
+        arr.select {|item| item.split("").sort == @matches}
+    end
+
+end
