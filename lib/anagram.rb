695186b480db02b4a323f694cac6690400cc27ba
oli-gra <oliver.grant@gmail.com>
2019-03-17 08:23:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require'pry'
+
+class Anagram
+
+def initialize(word)
+  @word = word
+  @words = []
+end
+
+def match(anagrams)
+  anagrams.each do|anagram|
+    if anagram.chars.sort == @word.chars.sort
+      @words << anagram
+    end
+  end
+  @words
+end
+
+
+
+end
